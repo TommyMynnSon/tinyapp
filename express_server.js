87ea1a5fadd1732720f6a00773bc14d314e56bf2
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-27 18:51:13 -0700

    Implement generateRandomString function

--- express_server.js
+++ express_server.js
@@ -5,6 +5,52 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+const generateRandomString = () => {
+  // (Type 1) ASCII value bounds for uppercase letters
+  const upperCaseStart = 65;
+  const upperCaseEnd = 90;
+
+  // (Type 2) ASCII value bounds for lowercase letters
+  const lowerCaseStart = 97;
+  const lowerCaseEnd = 122;
+
+  // (Type 3) ASCII value bounds for the numbers 0 - 9
+  const numberStart = 48;
+  const numberEnd = 57;
+
+  let randomString = '';
+
+  for (let i = 0; i < 6; i++) {
+    // Pick random type
+    const min = 1;
+    const max = 3
+
+    let type = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    let asciiCode;
+
+    // Pick random uppercase letter ASCII code 
+    if (type === 1) {
+      asciiCode = Math.floor(Math.random() * (upperCaseEnd - upperCaseStart + 1)) + upperCaseStart
+    }
+
+    // Pick random lowercase letter ASCII code
+    if (type === 2) {
+      asciiCode = Math.floor(Math.random() * (lowerCaseEnd - lowerCaseStart + 1)) + lowerCaseStart;
+    }
+
+    // Pick random number ASCII code
+    if (type === 3) {
+      asciiCode = Math.floor(Math.random() * (numberEnd - numberStart + 1)) + numberStart;
+    }
+
+    randomString += String.fromCharCode(asciiCode);
+  }
+
+  console.log(randomString);
+  return randomString;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
