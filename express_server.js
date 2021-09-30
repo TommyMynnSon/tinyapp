c1936cba204e46cb1750bf42512ff4471360186c
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-30 15:24:16 -0700

    Let / endpoint equal to /urls endpoint

--- express_server.js
+++ express_server.js
@@ -5,6 +5,13 @@ const bcryptjs = require('bcryptjs');
 const morgan = require('morgan');
 
 
+// [ Helper Functions ]
+const { generateRandomString } = require('./helpers/generateRandomString');
+const { isNewEmail } = require('./helpers/isNewEmail');
+const { getUserByEmail } = require('./helpers/getUserByEmail');
+const { urlsForUser } = require('./helpers/urlsForUser');
+
+
 // [ Server Setup ]
 const app = express();
 const PORT = 8080;
@@ -18,107 +25,6 @@ const users = {
 
 const urlDatabase = {
 
-};
-
-
-// [ Functions ]
-/**
- * Generates an alphanumeric string of length 6
- * @returns {string}
- */
-const generateRandomString = () => {
-  // (Type 1) HTML code value bounds for uppercase letters
-  const upperCaseStart = 65;
-  const upperCaseEnd = 90;
-
-  // (Type 2) HTML code value bounds for lowercase letters
-  const lowerCaseStart = 97;
-  const lowerCaseEnd = 122;
-
-  // (Type 3) HTML code value bounds for the numbers 0 - 9
-  const numberStart = 48;
-  const numberEnd = 57;
-
-  let randomString = '';
-
-  for (let i = 0; i < 6; i++) {
-    // Pick random Type
-    const min = 1;
-    const max = 3;
-
-    const type = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    // Random HTML code
-    let htmlCode;
-
-    // Pick random uppercase letter HTML code
-    if (type === 1) {
-      htmlCode = Math.floor(Math.random() * (upperCaseEnd - upperCaseStart + 1)) + upperCaseStart;
-    }
-
-    // Pick random lowercase letter HTML code
-    if (type === 2) {
-      htmlCode = Math.floor(Math.random() * (lowerCaseEnd - lowerCaseStart + 1)) + lowerCaseStart;
-    }
-
-    // Pick random number HTML code
-    if (type === 3) {
-      htmlCode = Math.floor(Math.random() * (numberEnd - numberStart + 1)) + numberStart;
-    }
-
-    randomString += String.fromCharCode(htmlCode);
-  }
-
-  return randomString;
-};
-
-/**
- * Checks if email has already been used to register
- * @param {string} email 
- * @param {object} database
- * @returns {boolean}
- */
-const isNewEmail = (email, database) => {
-  for (const user in database) {
-    if (database[user].email === email) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-/**
- * Gets user that owns email
- * @param {string} email 
- * @param {object} database 
- * @returns {object}
- */
-const getUserByEmail = (email, database) => {
-  for (const user in database) {
-    if (database[user].email === email) {
-      return database[user];
-    }
-  }
-
-  return undefined;
-};
-
-/**
- * Get URLs created by user with id
- * @param {*} id 
- * @returns {object}
- */
-const urlsForUser = (id, database) => {
-  let urls = {};
-
-  for (const shortURL in database) {
-    if (database[shortURL]["userId"] === id) {
-      urls[shortURL] = database[shortURL].longURL;
-    }
-  }
-
-  return urls;
 };
 
 
@@ -141,7 +47,14 @@ app.get("/users.json", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const userId = req.session.userId;
+  const user = users[userId];
+
+  let urls = urlsForUser(userId, urlDatabase);
+
+  const templateVars = { urls, user };
+
+  res.render(`urls_index`, templateVars);
 });
 
 app.get("/urls", (req, res) => {
