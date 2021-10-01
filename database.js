0ce223dec445e46131d07aaee5217cce0905a718
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-30 18:33:48 -0700

    Separate database objects from express_server.js

---
+++ database.js
@@ -0,0 +1,23 @@
+const usersDatabase = {
+  // Expected structure:
+  //
+  // "oJeZw9": {
+  //   "id": "oJeZw9",
+  //   "email": "example@gmail.com",
+  //   "hashedPassword": "$2a$10$smzP9l8KoAoEGAZIFSV/neBJz8v/s748cdBiYln.6W9PgYDsTGb1a"
+  // }
+};
+
+const urlDatabase = {
+  // Expected structure:
+  //
+  // "CMIM2k": {
+  //   "longURL": "http://www.example.com",
+  //   "userId": "L890c2"
+  // }
+};
+
+module.exports = {
+  usersDatabase,
+  urlDatabase
+};
